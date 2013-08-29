48e2774f26a4609699028367aac58a4532817df1
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-08-28 17:18:55 -0700

    all files

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,82 @@
+var http = require('http');
 var path = require('path');
+var fs = require('fs');
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
-module.exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+var handleRequest = function (req, res) {
+  if(req.method === 'GET'){
+    if(req.url === '/' || req.url === '/styles.css'){
+      handleStaticRequests(req, res);
+    } else{
+       handleGet(req, res);
+    }
+  } else if(req.method === 'POST'){
+    handlePost(req, res);
+  }
 };
+
+var handleGet = function(request, response){
+  var htmlFile = fs.createReadStream('../data/sites.txt');
+  htmlFile.pipe(response);
+};
+
+var handlePost = function(request, response){
+  var result = '';
+  request.on('data', function(chunk){
+    result+=chunk;
+  });
+  request.on('end', function(){
+    var parsedData = result.toString();
+    fs.writeFile('../data/sites.txt', parsedData, function(err){
+      if(err){
+        console.log('there was an error');
+        response.writeHead(404);
+      } else{
+        console.log('Successfully wrote to file');
+      }
+    });
+  });
+};
+
+var handleStaticRequests = function(request, response) {
+  var filePath = request.url;
+  if (filePath === '/') {
+    filePath = '/index.html';
+  } 
+  
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+    contentType = 'text/javascript';
+    break;
+    case '.css':
+    contentType = 'text/css';
+    break;
+  }
+
+  fs.exists( __dirname + '/public' + filePath, function(exists){
+    if(exists) {
+      fs.readFile( __dirname + '/public' + filePath, function(error, content) {
+        if (error) {
+          response.writeHead(500);
+          response.end();
+        }
+        else {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content.toString());
+        }
+      });
+    }
+    else {
+      response.writeHead(404);
+      response.end();
+    }
+  });
+};
+
+
+exports.handleRequest = handleRequest;
+exports.handleGet = handleGet;
+exports.handlePost = handlePost;
+exports.handleStaticRequests = handleStaticRequests;
